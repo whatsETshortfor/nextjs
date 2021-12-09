a55c6fc32ace992e48828138ac9a3dc3281f4e7e
Matthew Colman <Matthew.Colman@rcn.org.uk>
2021-12-09 12:30:38 +0000

    sort nextjs reference after refactor

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
 import { Layout } from "../components/Layout";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.css";
+import { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
